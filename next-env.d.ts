49137c039a0d8093845dfe360890589b4b0b7749
Basit <malikbasitmaqsood@gmail.com>
2020-10-29 12:23:27 +0500

    initial push

---
+++ next-env.d.ts
@@ -0,0 +1,4 @@
+import { type } from "os";
+
+// <reference types="next" />
+// <reference types="next/types/global" />
---
+++ pages/nicepage.tsx
@@ -0,0 +1,5 @@
+const NicePage = () => {
+    return (<h1>Nice Page</h1>);
+}
+
+export default NicePage;
